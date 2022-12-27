d2c5637da500d92d5b1cd2fcf41361e404c0bba6
Boris Garcés Vernier <83401906+Borisgarces@users.noreply.github.com>
2022-12-27 11:49:43 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-
-/*80. Crea una variante del programa anterior, que pregunte al usuario cuántos 
+﻿/*80. Crea una variante del programa anterior, que pregunte al usuario cuántos 
 datos guardará en un primer bloque de números reales de doble precisión, 
 luego cuántos datos guardará en un segundo bloque, y finalmente pida todos esos datos. 
 Los debe guardar en un array de arrays. Después mostrará el promedio de los valores que 
 hay guardados en el primer subarray, luego el promedio de los valores en el segundo subarray 
-y finalmente el promedio global.
-Miguel Angel (...)
-*/
-
-
+y finalmente el promedio global.*/
 using System;
 using System.Linq;
 
@@ -21,8 +16,10 @@ namespace Ejercicio_80
 			byte lineas = 0;
 			double capacidad = 0;
 			double suma = 0;
-			double promedio=0;
-
+			double valor = 0;
+			double sumaglobal = 0;
+			double promedio = 0;
+            double promedioglobal=0;
 
 			Console.WriteLine();
 			Console.Write("Cuantas líneas tendra la tabla: ");
@@ -58,9 +55,9 @@ namespace Ejercicio_80
 
 
 
-			for (int i = 0; i < numeros.Length; i++)
+			for (int i = 0; i < lineas; i++)
 			{
-				for (int j = 0; j < numeros[i].Length; j++)
+				for (int j = 0; j < capacidad; j++)
 				{
 
 					Console.Write("{0} ", numeros[i][j]);
@@ -72,6 +69,7 @@ namespace Ejercicio_80
 
 
 
+			// suma de cada línea		
 
 			for (int i = 0; i < lineas; i++)
 			{
@@ -82,7 +80,7 @@ namespace Ejercicio_80
 
 					suma += numeros[i][j];
 					numeros[i][j] = suma;
-			
+
 
 				}
 				suma = 0;
@@ -90,24 +88,60 @@ namespace Ejercicio_80
 			}
 
 
-			
+
+
+
 			for (int i = 0; i < lineas; i++)
 			{
 
 				for (int j = 0; j < capacidad; j++)
 				{
 
-					suma=numeros[i][j];
-					promedio=suma/capacidad;
-					
+					suma = numeros[i][j];
+
 				}
-				Console.WriteLine("El promedio de la línea {0} es {1}",i+1,promedio);
+				Console.WriteLine("La suma de la línea{0} es {1}", i + 1, suma);
+			}
+			Console.WriteLine();
+
+
+
+			//promedio de cada línea
+
+			for (int i = 0; i < lineas; i++)
+			{
+
+				for (int j = 0; j < capacidad; j++)
+				{
+
+					suma = numeros[i][j];
+					promedio = suma / capacidad;
+
+				}
+				Console.WriteLine("El promedio de la línea {0} es {1}", i + 1, promedio);
 			}
 
 
 
+			// suma y promedio global
+
+			for (int i = 0; i < lineas; i++)
+			{
+
+				for (int j = 0; j < lineas; j++)
+				{
+
+					valor = numeros[i][j];
+					sumaglobal = sumaglobal + valor;
+					promedioglobal=sumaglobal/lineas;
+
+				}
+				Console.WriteLine();
+			}
 
 
+			Console.WriteLine("La suma global es {0}", sumaglobal);
+			Console.WriteLine("El promedio global es {0}", promedioglobal);
 
 		}
 	}
